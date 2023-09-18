3ad0510e96f3c34c265037c7eeb3d84e093b94e8
Neeyo1 <jakub.nycz99@gmail.com>
2023-09-18 13:15:42 +0200

    Player movement on right mouse button click

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public float boundY = 10.0f;
     public Rigidbody2D rb;
     public bool canMove = true;
+    private Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +23,22 @@ public class PlayerMovement : MonoBehaviour
     {
         if(canMove)
         {
-            horizontalInput = Input.GetAxis("Horizontal");
-            verticalInput = Input.GetAxis("Vertical");
-            transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
-            transform.Translate(Vector2.up * verticalInput * Time.deltaTime * speed);
+            //horizontalInput = Input.GetAxis("Horizontal");
+            //verticalInput = Input.GetAxis("Vertical");
+            //transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
+            //transform.Translate(Vector2.up * verticalInput * Time.deltaTime * speed);
+
+            if (Input.GetMouseButton(1))
+            {
+                target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                target.z = transform.position.z;
+                float distanceToTarget = Vector2.Distance(target, new Vector2(transform.position.x, transform.position.y));
+                if(distanceToTarget <= 0.5)
+                {
+                    target = transform.position;
+                }
+		    }
+		    transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
         }
         
         if(transform.position.x <= -boundX)
